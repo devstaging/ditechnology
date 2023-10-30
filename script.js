1a273ae3086c53ee367e39dc699099683fe34928
devstaging <149357272+devstaging@users.noreply.github.com>
2023-10-30 07:09:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+jQuery(function($){
+  $('.absolute-wrapper .path-class-all').on( "mouseover", function() {
+    const currentPath = $(this).attr('id');
+    console.log(currentPath);
+
+    $('.members-box').removeClass('active');
+    $('.members-box.' + currentPath).addClass('active');
+  });
+  
+   $('.absolute-wrapper .path-class-all').on( "mouseout", function(){
+     $('.members-box').removeClass('active');
+   });
+});
